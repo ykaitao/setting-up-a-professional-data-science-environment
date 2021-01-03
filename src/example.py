9572d7fcb2485a2f2895cdb3b44f268b3ac694d5
ykaitao <ykaitao@gmail.com>
2021-01-03 13:10:23 +0100

    examples added

--- src/example.py
+++ src/example.py
@@ -1,4 +1,42 @@
+from typing import List, Set, Tuple, Dict
+
 import numpy as np
 import pandas as pd
 
-name_dict = {"First name": ["K."], "Last name": ["Yang"]}
+
+def add_simple(a, b):
+    return a + b
+
+
+def add_with_type_hints(a: int, b: int) -> int:
+    return a + b
+
+
+def combine_with_docstring(a: int, b: int) -> List[int]:
+    """Conbine two integers into a list.
+
+    Args:
+        a (int): integer to be combined.
+        b (int): integer to be combined.
+
+    Returns:
+        List[int]: A list of conbined integer.
+    """
+    return [a, b]
+
+
+def combine_with_example(a: int, b: int) -> List[int]:
+    """Conbine two integers into a list.
+
+    Args:
+        a (int): integer to be combined.
+        b (int): integer to be combined.
+
+    Returns:
+        List[int]: A list of conbined integer.
+
+    Examples:
+    >>> combine_with_example(1, 2)
+    [1, 2]
+    """
+    return [a, b]
